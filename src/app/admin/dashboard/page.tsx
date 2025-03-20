c55d109cafddb18fd0afb08fe19c9a2e74fe5afd
ryangualberto <ryanoliveiragualberto@gmail.com>
2025-03-20 13:47:55 -0300

    feat: adicionar exibição de contagem de produtos, categorias e variantes no dashboard

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,6 +1,11 @@
+"use client";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { listCategories } from "@/requests/category";
+import { listProducts } from "@/requests/product";
+import { listProductVariants } from "@/requests/product-variant";
+import { useQuery } from "@tanstack/react-query";
 import {
   Package,
   ShoppingBag,
@@ -72,6 +77,21 @@ const pedidosEntrega = [
 ];
 
 export default function Dashboard() {
+  const { data: products } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => await listProducts(),
+  });
+
+  const { data: productVariants } = useQuery({
+    queryKey: ["variants"],
+    queryFn: async () => await listProductVariants(),
+  });
+
+  const { data: categories } = useQuery({
+    queryKey: ["categories"],
+    queryFn: async () => await listCategories(),
+  });
+
   return (
     <div className="space-y-6">
       <div>
@@ -105,9 +125,15 @@ export default function Dashboard() {
             <Package className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">0</div>
+            <div className="text-2xl font-bold">{products?.length || 0}</div>
             <p className="text-xs text-muted-foreground">
-              Nenhum produto cadastrado
+              {products?.length === 0 ? (
+                <Link href="/admin/products">
+                  <Button variant="link" className="p-0">
+                    Cadastrar um produto
+                  </Button>
+                </Link>
+              ) : null}
             </p>
           </CardContent>
         </Card>
@@ -120,9 +146,15 @@ export default function Dashboard() {
             <Tag className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">0</div>
+            <div className="text-2xl font-bold">{categories?.length || 0}</div>
             <p className="text-xs text-muted-foreground">
-              Nenhuma categoria cadastrada
+              {categories?.length === 0 ? (
+                <Link href="/admin/categories">
+                  <Button variant="link" className="p-0">
+                    Cadastrar uma categoria
+                  </Button>
+                </Link>
+              ) : null}
             </p>
           </CardContent>
         </Card>
@@ -135,9 +167,17 @@ export default function Dashboard() {
             <Layers className="h-4 w-4 text-muted-foreground" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">0</div>
+            <div className="text-2xl font-bold">
+              {productVariants?.length || 0}
+            </div>
             <p className="text-xs text-muted-foreground">
-              Nenhuma variante cadastrada
+              {productVariants?.length === 0 ? (
+                <Link href="/admin/variants">
+                  <Button variant="link" className="p-0">
+                    Cadastrar uma variante
+                  </Button>
+                </Link>
+              ) : null}
             </p>
           </CardContent>
         </Card>
