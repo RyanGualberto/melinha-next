84ca765d20a5c771ac4c896d8988bb6d5ba4ae94
ryangualberto <ryanoliveiragualberto@gmail.com>
2025-03-20 23:56:50 -0300

    feat: atualizar layout para português e remover fontes não utilizadas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "@/contexts/query-context";
 import { AuthenticatedContextProvider } from "@/contexts/user-context";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="pt-BR">
+      <body className={`antialiased`}>
         <QueryProvider>
           <AuthenticatedContextProvider>
             {children}
