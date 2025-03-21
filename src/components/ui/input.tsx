8bf13647e1fa096f9f8c2b9563f17095d56602ff
ryangualberto <ryanoliveiragualberto@gmail.com>
2025-03-21 19:20:14 -0300

    feat: adicionar funcionalidade de mostrar/esconder senha no campo de entrada

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
+import { Button } from "./button";
+import { Eye, EyeClosed } from "lucide-react";
 
 function Input({
   className,
@@ -10,13 +12,14 @@ function Input({
 }: React.ComponentProps<"input"> & {
   icon?: React.ReactNode;
 }) {
+  const [hidePassword, setHidePassword] = React.useState(false);
   return (
     <div className="relative">
       {icon && (
         <div className="absolute left-3 top-1/2 -translate-y-1/2 ">{icon}</div>
       )}
       <input
-        type={type}
+        type={type === "password" ? (hidePassword ? "password" : "text") : type}
         data-slot="input"
         className={cn(
           "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
@@ -29,6 +32,17 @@ function Input({
         )}
         {...props}
       />
+      {type === "password" && (
+        <Button
+          size="icon"
+          variant="ghost"
+          className="absolute right-3 top-1/2 -translate-y-1/2"
+          type="button"
+          onClick={() => setHidePassword(!hidePassword)}
+        >
+          {hidePassword ? <EyeClosed /> : <Eye />}
+        </Button>
+      )}
     </div>
   );
 }
