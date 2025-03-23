9082e9706554da2e0ea3db2a0e84c6bd10d926f2
ryangualberto <ryanoliveiragualberto@gmail.com>
2025-03-23 14:35:46 -0300

    feat: adicionar suporte a PWA com configuração de manifest e ícones

--- next.config.ts
+++ next.config.ts
@@ -14,4 +14,12 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
+export default withPWA({
+  ...nextConfig,
+});
